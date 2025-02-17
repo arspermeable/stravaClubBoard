2288bae05262f0355f9ecced169156cf0afc2356
arspermeable <efraim.martinez@gmail.com>
2025-02-17 16:49:45 +0000

    test version 8.1

--- pages/authorize.py
+++ pages/authorize.py
@@ -4,7 +4,7 @@ from streamlit_oauth import OAuth2Component
 STRAVA_CLIENT_ID = st.secrets.strava.client_id
 STRAVA_CLIENT_SECRET = st.secrets.strava.client_secret
 
-STRAVA_SCOPE = ["activity:read_all"]
+STRAVA_SCOPE = ["read"]
 
 STRAVA_REDIRECT_URL = st.secrets.app.url + "/return"
 STRAVA_AUTHORIZATION_URL = "https://www.strava.com/oauth/authorize"
