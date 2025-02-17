3da24a25b9534de4b80cfe457ccace315fbec784
arspermeable <efraim.martinez@gmail.com>
2025-02-17 14:50:06 +0000

    test version 4 with pages

---
+++ pages/return.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(page_title="Return fake")
+
+st.markdown("# Return Fake")
+
+st.write(
+    """This is a fake page to simulate the return after authorization."""
+)
