f31745202500ac8384701d630041f8b921d5dc4c
arspermeable <efraim.martinez@gmail.com>
2025-02-17 17:00:48 +0000

    test version 8.1

--- pages/authorize.py
+++ pages/authorize.py
@@ -33,6 +33,7 @@ if 'token' not in st.session_state:
                                                   extras_params={"approval_prompt":"force"})
     st.write("fila 1")
     if result and 'token' in result:
+        st.write("fila 2")
         # If authorization successful, save token in session state
         st.session_state.token = result.get('token')
         st.rerun()
