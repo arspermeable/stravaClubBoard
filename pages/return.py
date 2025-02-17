749d868b5ad23a60b09cf581e9dd69b49f788015
arspermeable <efraim.martinez@gmail.com>
2025-02-17 15:00:46 +0000

    test version 5

--- pages/return.py
+++ pages/return.py
@@ -7,3 +7,6 @@ st.markdown("# Return Fake")
 st.write(
     """This is a fake page to simulate the return after authorization."""
 )
+
+st.write(st.query_params["code"])
+st.write(st.query_params["scope"])
