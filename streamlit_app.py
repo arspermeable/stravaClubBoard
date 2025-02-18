136028afda6f1a31fc774d60955730d68085cc6d
arspermeable <efraim.martinez@gmail.com>
2025-02-18 00:32:25 +0000

    Test c12

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,7 +55,7 @@ else:
     token = st.session_state['token']
     #st.json(token)
 #    st.write(token)
-    st.write("You are authorized until " + token['expires_at'])
+    st.write("You are authorized until " + str(token['expires_at']))
     st.write(token['athlete']['firstname'] + " " + token['athlete']['lastname'])
     if st.button("Refresh Token"):
         st.write("fila 4")
